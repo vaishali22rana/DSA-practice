a48f5e4f9fbd2c21fb12c2fb3c5ddfecfda95ba1
Vaishali <lit2017@iiiitl.ac..in>
2021-05-26 01:39:26 +0530

    First commit

---
+++ twosum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void printVector(vector < int > v) {
+  int n = v.size();
+  for (auto x: v) {
+    cout << x << " ";
+  }
+}
+vector < int > scanVector(int n) {
+  int x;
+  vector < int > v;
+  for (int i = 0; i < n; i++) {
+    cin >> x;
+    v.push_back(x);
+  }
+  return v;
+}
+int scanNum() {
+  int n;
+  cin >> n;
+  return n;
+}
+int main() {
+  int n = scanNum();
+  vector < int > v = scanVector(n);
+    
+  return 0;
+}
